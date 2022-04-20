70fe861ff5db734bbe5a84af4439c26ad7ddc7cb
Tomas Millan Lanhozo <tomas.millan96@gmail.com>
2022-04-19 23:58:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var x = document.getElementById("main-nav");
+    if (x.style.display === "grid") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "grid";
+    }
+  } 
+  
